adaf62b01130752c65808a153bd255fbffe9cb9c
César Pomar <cesarpomar18@gmail.com>
2025-03-26 23:02:54 +0100

    multiplatform thread_local fix

--- omp4py/cruntime/basics/threadlocal.pyx
+++ omp4py/cruntime/basics/threadlocal.pyx
@@ -3,8 +3,29 @@ from cpython.object cimport PyObject
 
 _storage: local = local()
 
-cdef extern from '<threads.h>':
+cdef extern from *:
     """
+    // https://stackoverflow.com/questions/18298280/how-to-declare-a-variable-as-thread-local-portably
+    #ifndef thread_local
+    # if __STDC_VERSION__ >= 201112 && !defined __STDC_NO_THREADS__
+    #  define thread_local _Thread_local
+    # elif defined _WIN32 && ( \
+           defined _MSC_VER || \
+           defined __ICL || \
+           defined __DMC__ || \
+           defined __BORLANDC__ )
+    #  define thread_local __declspec(thread)
+    /* note that ICC (linux) and Clang are covered by __GNUC__ */
+    # elif defined __GNUC__ || \
+           defined __SUNPRO_C || \
+           defined __hpux || \
+           defined __xlC__
+    #  define thread_local __thread
+    # else
+    #  error "Cannot define thread_local"
+    # endif
+    #endif
+
     thread_local PyObject* omp_thread = NULL;
     """
     PyObject *omp_thread
