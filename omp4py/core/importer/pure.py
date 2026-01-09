457e091640bdd8149e83172d4253ff65f704564f
César Pomar <cesarpomar18@gmail.com>
2026-01-09 15:58:39 +0100

    Remove Cython dependency in pure mode

--- omp4py/core/importer/pure.py
+++ omp4py/core/importer/pure.py
@@ -1,18 +1,19 @@
 import sys
 import os
-import importlib.machinery
+from importlib.machinery import PathFinder, ExtensionFileLoader, SourceFileLoader
+import omp4py.core.importer.cython
 
+__all__ = []
 
 class PureImporter:
 
     @staticmethod
     def find_spec(name, import_path, target_module):
         if "omp4py.runtime" in name:
-            from importlib.machinery import PathFinder
             spec = PathFinder.find_spec(name, import_path, target_module)
-            if isinstance(spec.loader, importlib.machinery.ExtensionFileLoader):
+            if isinstance(spec.loader, ExtensionFileLoader):
                 pure_file: str = os.path.join(os.path.dirname(spec.origin), spec.name[len(spec.parent) + 1:] + '.py')
-                spec.loader = importlib.machinery.SourceFileLoader(name, pure_file)
+                spec.loader = SourceFileLoader(name, pure_file)
                 spec.origin = pure_file
 
             return spec
@@ -20,3 +21,5 @@ class PureImporter:
 
 
 sys.meta_path.insert(0, PureImporter())
+if "cython" not in sys.modules:
+    sys.modules["cython"] = omp4py.core.importer.cython
